78031ac6b4ffd9fe4fd09669b003b534bf2125b3
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-05-30 21:44:35 +0400

    task correct

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,38 +1,16 @@
-
-// const inputData = {
-//     name : {
-//         class : 'input-style',
-//         placeholder : 'Name',
-//     },
-//     email : {
-//         class : 'input-style',
-//         placeholder : 'Email',
-//     },
-//     messages : {
-//         class : 'input-style',
-//         placeholder : 'messages',
-//     },
+import ContactItem from "./ContactItem"
 
 
 
 
-export default function Contact(props){
-    // const item = inputData[props.type];
+export default function Contact(){
+
     return(
         <div className="contactWrapper">
             <h2 className="aboutUs">Contact</h2>
-            <div className="inputConteiner">
-                <label htmlFor="">Name</label>
-                <input type="text" placeholder="Name" />
-            </div>
-            <div className="inputConteiner">
-                <label htmlFor="">Email</label>
-                <input type="text" placeholder="Email" />
-            </div>
-            <div className="inputConteiner">
-                <label htmlFor="">Message</label>
-                <input type="text" placeholder="Message" />
-            </div>
+            <ContactItem type="name"/>
+            <ContactItem type="email"/>
+            <ContactItem type="messages" />
             <div className="buttonConteiner">
                 <button className="forButtons">Send</button>
             </div>
---
+++ src/Components/contact/ContactItem.jsx
@@ -0,0 +1,28 @@
+
+const inputData = {
+    name : {
+        class : 'input-style',
+        placeholder : 'Name',
+    },
+    email : {
+        class : 'input-style',
+        placeholder : 'Email',
+    },
+    messages : {
+        class : 'input-style',
+        placeholder : 'messages',
+    },
+}   
+
+
+export default function ContactItem(props){
+    const item = inputData[props.type];
+    return(
+        <>
+            <div className="inputConteiner">
+                <label htmlFor="">{item.placeholder}</label>
+                <input type="text" placeholder={item.placeholder}/>
+            </div>
+        </>
+    )
+}
